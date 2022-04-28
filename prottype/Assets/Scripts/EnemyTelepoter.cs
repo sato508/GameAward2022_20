a93100d0e760916b43b36f6a4be55f2e390da380
harukei <58187672+hakiyoshi@users.noreply.github.com>
2022-04-28 22:43:49 +0900

    初期位置が変わるようにした

---
+++ prottype/Assets/Scripts/EnemyTelepoter.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTelepoter : MonoBehaviour
+{
+    public GameObject Enemy;
+
+    public Transform[] TeleportPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int random = Random.Range(0, TeleportPosition.Length - 1);
+        Transform enemytransform = Enemy.transform;
+        enemytransform.position =
+            new Vector3(TeleportPosition[random].position.x, 5.0f, TeleportPosition[random].position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
